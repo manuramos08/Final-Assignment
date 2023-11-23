7a15de1f0ac654cf4253eca5fad6059af8905748
Manuel Ramos <106841716+manuramos08@users.noreply.github.com>
2023-11-23 20:10:44 -0300

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, we will learn to: Create a Jupyter Notebook, Insert code and markdown cells, and Share your notebook through GitHub
+
+# Some of the popular languages that Data Scientists use are:
+# 1) Phyton 
+# 2) R 
+# 3) Sql 
+# 4) Scala
+# 5) Java
+# 6) C++
+# 
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1) Pandas
+# 2) NumPy
+# 3) MatPlotlib
+# 4) Seaborn
+# 5) TensorFlow
+# 6) PyTorch 
+# 
+
+# |Data Science Tools|
+# |------------------|
+# | Jupyter notebooks |
+# | R studio |
+# | Apache Spyder | 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+# 
+
+# In[5]:
+
+
+# This a simple arithmetic expression to mutiply then add integers
+(3*4)+5
+
+
+# In[6]:
+
+
+#  This will convert 200 minutes to hours by diving by 60
+200 / 60
+
+
+# **Objectives:** 
+# - Headings
+# - Creating markdown and code cells
+# - Simple arithmetic expressions
+# - Divisions 
+
+# ## Author
+# Manuel Ramos
